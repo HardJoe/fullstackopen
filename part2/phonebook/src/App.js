9c9a7477b0d83ce5bd5d0f78b9e98dbb86858edc
HardJoe <jonathanamadeus95@gmail.com>
2022-02-18 22:26:31 +0700

    Implement phonebook step3 (2.8)

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,14 +3,16 @@ import { isEqual } from 'lodash'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567'}
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     const newPerson = {
       name: newName,
+      number: newNumber,
     }
     
     let isFound = false
@@ -24,11 +26,16 @@ const App = () => {
     if (!isFound) {
       setPersons(persons.concat(newPerson))
       setNewName('')
+      setNewNumber('')
     }
   }
 
-  const handlePersonChange = (event) => {
+  const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -36,15 +43,19 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: 
-          <input value={newName} onChange={handlePersonChange} />
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      {persons.map(person => 
+        <p key={person.name}>{person.name} {person.number}</p>
+      )}
     </div>
   )
 }

a858bcfc0f54f45677921622c85b23d7d23f2a85
HardJoe <jonathanamadeus95@gmail.com>
2022-02-20 10:27:49 +0700

    Implement phonebook step6 (2.11)

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({ query, handleQueryChange }) => (
   <div>
@@ -34,12 +35,7 @@ const Persons = ({ searchResult }) => (
 )
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [query, setQuery] = useState('')
@@ -84,6 +80,14 @@ const App = () => {
     ))
     setSearchResult(result)
   }, [persons, query])
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   return (
     <div>

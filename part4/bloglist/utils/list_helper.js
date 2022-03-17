f632788769c5ff1eee00fcada6dc1b8a74549668
HardJoe <jonathanamadeus95@gmail.com>
2022-03-17 09:35:41 +0700

    Implement helper functions and unit tests step3 (4.5)

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -10,7 +10,20 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return {};
+  } else {
+    const max = blogs.reduce((prev, cur) =>
+      prev.likes > cur.likes ? prev : cur
+    );
+    const { _id, url, __v, ...res } = max;
+    return res;
+  }
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };

2e6bfd52a289377044b4d88384e6b5fb08668acd
HardJoe <jonathanamadeus95@gmail.com>
2022-03-18 11:39:27 +0700

    Implement blog list expansions step1 (4.13)

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -8,16 +8,20 @@ const api = supertest(app);
 
 const initialBlogs = [
   {
+    _id: '5a422a851b54a676234d17f7',
     title: 'React patterns',
     author: 'Michael Chan',
     url: 'https://reactpatterns.com/',
     likes: 7,
+    __v: 0,
   },
   {
+    _id: '5a422aa71b54a676234d17f8',
     title: 'Go To Statement Considered Harmful',
     author: 'Edsger W. Dijkstra',
     url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
     likes: 5,
+    __v: 0,
   },
 ];
 
@@ -89,6 +93,20 @@ describe('post blogs', () => {
     };
 
     await api.post('/api/blogs').send(newBlog).expect(400);
+  });
+});
+
+describe('delete blogs', () => {
+  test('a valid blog can be deleted', async () => {
+    await api.delete('/api/blogs/5a422a851b54a676234d17f7').expect(204);
+
+    const response = await api.get('/api/blogs');
+
+    expect(response.body).toHaveLength(initialBlogs.length - 1);
+  });
+
+  test('invalid id returns bad request', async () => {
+    await api.delete('/api/blogs/123123').expect(400);
   });
 });
 

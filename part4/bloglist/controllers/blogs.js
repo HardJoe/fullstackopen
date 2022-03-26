022e106471cac78139dbbf8942e1ce38a454c87f
HardJoe <jonathanamadeus95@gmail.com>
2022-03-26 11:17:18 +0700

    Implement bloglist expansion step8 (4.20)

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -8,30 +8,22 @@ blogsRouter.get('', async (request, response) => {
   response.json(blogs);
 });
 
-const getTokenFrom = (request) => {
-  const authorization = request.get('authorization');
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7);
-  }
-  return null;
-};
-
 blogsRouter.post('', async (request, response) => {
-  const token = getTokenFrom(request);
-  const decodedToken = jwt.verify(token, process.env.SECRET);
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token missing or invalid' });
+  try {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET);
+    const user = await User.findById(decodedToken.id);
+
+    const blog = new Blog(request.body);
+    blog.user = user._id;
+    const result = await blog.save();
+
+    user.blogs = user.blogs.concat(blog._id);
+    await user.save();
+
+    response.status(201).json(result);
+  } catch {
+    response.status(401).json({ error: 'token missing or invalid' });
   }
-  const user = await User.findById(decodedToken.id);
-
-  const blog = new Blog(request.body);
-  blog.user = user._id;
-  const result = await blog.save();
-
-  user.blogs = user.blogs.concat(blog._id);
-  await user.save();
-
-  response.status(201).json(result);
 });
 
 blogsRouter.put('/:id', async (request, response) => {

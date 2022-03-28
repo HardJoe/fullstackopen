c5647b88b181506bca4a553253372c9500b48b9f
HardJoe <jonathanamadeus95@gmail.com>
2022-03-28 16:04:49 +0700

    Implement bloglist tests step2 (5.14)

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Blog from './Blog';
 
 test('renders content when details are hidden', () => {
@@ -23,4 +24,36 @@ test('renders content when details are hidden', () => {
 
   expect(container).not.toHaveTextContent('https://reactpatterns.com/');
   expect(container).not.toHaveTextContent('7');
+});
+
+test('clicking the button renders details', async () => {
+  const blog = {
+    id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    user: {
+      id: '623fe6e2e41511ec21543037',
+      username: 'jmay',
+      name: 'James May',
+    },
+  };
+
+  const user = {
+    username: 'jmay',
+    name: 'James May',
+    token: 'thisisatoken',
+  };
+
+  const { container } = render(<Blog blog={blog} user={user} />);
+
+  const button = screen.getByText('view');
+  userEvent.click(button);
+
+  expect(container).toHaveTextContent('React patterns');
+  expect(container).toHaveTextContent('Michael Chan');
+
+  expect(container).toHaveTextContent('https://reactpatterns.com/');
+  expect(container).toHaveTextContent('7');
 });

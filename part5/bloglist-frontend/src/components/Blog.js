7bb1cfc3057290cbeafa101e30affcb68320418e
HardJoe <jonathanamadeus95@gmail.com>
2022-03-28 12:29:04 +0700

    Implement bloglist frontend step7 (5.7)

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,46 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} by {blog.author}
-  </div>
-);
+import { useState } from 'react';
+
+const Blog = ({ key, blog }) => {
+  const [viewed, setViewed] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const handleViewClick = () => {
+    setViewed(!viewed);
+  };
+
+  if (!viewed) {
+    return (
+      <div style={blogStyle}>
+        {blog.title} by {blog.author}
+        <button value={key} onClick={handleViewClick}>
+          view
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}
+        <button value={key} onClick={handleViewClick}>
+          hide
+        </button>
+      </div>
+      {blog.url}
+      <div>
+        likes {blog.likes} <button>like</button>
+      </div>
+      {blog.author}
+    </div>
+  );
+};
 
 export default Blog;

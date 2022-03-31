9360374002914f07ca851042abdbdb27494ff3a4
HardJoe <jonathanamadeus95@gmail.com>
2022-03-31 15:31:14 +0700

    Implement anecdotes redux step1 (6.3)

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -19,11 +19,26 @@ const asObject = (anecdote) => {
 
 const initialState = anecdotesAtStart.map(asObject);
 
+export const vote = (id) => {
+  return {
+    type: 'VOTE',
+    data: { id }
+  };
+};
+
 const reducer = (state = initialState, action) => {
   console.log('state now: ', state);
   console.log('action', action);
 
-  return state;
+  switch (action.type) {
+    case 'VOTE':
+      const id = action.data.id;
+      const oldAnecdote = state.find(a => a.id === id);
+      const updatedAnecdote = { ...oldAnecdote, votes: oldAnecdote.votes + 1 };
+      return state.map(a => a.id === id ? updatedAnecdote : a);
+    default:
+      return state;
+  }
 };
 
 export default reducer;

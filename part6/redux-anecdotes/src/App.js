d8481f742ce20e9cc04bda3e70d486921436a712
HardJoe <jonathanamadeus95@gmail.com>
2022-03-31 16:12:12 +0700

    Implement anecdotes revisited step2 (6.4)

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,27 +1,34 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { vote } from './reducers/anecdoteReducer';
+import { createAnecdote, vote } from './reducers/anecdoteReducer';
 
 const App = () => {
-  const anecdotes = useSelector(state => state);
+  const anecdotes = useSelector((state) => state);
   const dispatch = useDispatch();
+
+  const addAnecdote = (event) => {
+    event.preventDefault();
+    const content = event.target.content.value;
+    event.target.content.value = '';
+    dispatch(createAnecdote(content));
+  };
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
-            <button onClick={() => dispatch((vote(anecdote.id)))}>vote</button>
+            <button onClick={() => dispatch(vote(anecdote.id))}>vote</button>
           </div>
         </div>
-      )}
+      ))}
       <h2>create new</h2>
-      <form>
-        <div><input /></div>
+      <form onSubmit={addAnecdote}>
+        <div>
+          <input name="content" />
+        </div>
         <button>create</button>
       </form>
     </div>
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -4,7 +4,7 @@ const anecdotesAtStart = [
   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
 ];
 
 const getId = () => (100000 * Math.random()).toFixed(0);
@@ -13,29 +13,39 @@ const asObject = (anecdote) => {
   return {
     content: anecdote,
     id: getId(),
-    votes: 0
+    votes: 0,
   };
 };
 
 const initialState = anecdotesAtStart.map(asObject);
 
+export const createAnecdote = (content) => {
+  return {
+    type: 'NEW_ANECDOTE',
+    data: {
+      content,
+      id: getId(),
+      votes: 0,
+    },
+  };
+};
+
 export const vote = (id) => {
   return {
     type: 'VOTE',
-    data: { id }
+    data: { id },
   };
 };
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state);
-  console.log('action', action);
-
   switch (action.type) {
+    case 'NEW_ANECDOTE':
+      return state.concat(action.data);
     case 'VOTE':
       const id = action.data.id;
-      const oldAnecdote = state.find(a => a.id === id);
+      const oldAnecdote = state.find((a) => a.id === id);
       const updatedAnecdote = { ...oldAnecdote, votes: oldAnecdote.votes + 1 };
-      return state.map(a => a.id === id ? updatedAnecdote : a);
+      return state.map((a) => (a.id === id ? updatedAnecdote : a));
     default:
       return state;
   }

c5af5db826fb55a9e210904ab3a960045d220b8c
HardJoe <jonathanamadeus95@gmail.com>
2022-04-16 12:38:45 +0700

    Implement books of an author (8.4)

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -107,7 +107,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -116,7 +116,7 @@ const resolvers = {
   Query: {
     authorCount: () => authors.length,
     bookCount: () => books.length,
-    allBooks: () => books,
+    allBooks: (root, args) => books.filter((b) => b.author === args.author),
     allAuthors: () => authors,
   },
   Author: {

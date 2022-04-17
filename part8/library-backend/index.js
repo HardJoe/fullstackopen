d9ab19db90de6f45da9aeccfa1ec293db7c197ff
HardJoe <jonathanamadeus95@gmail.com>
2022-04-17 09:57:25 +0700

    Implement adding a book (8.6)

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server');
+const { v1: uuid } = require('uuid');
 
 let authors = [
   {
@@ -110,6 +111,15 @@ const typeDefs = gql`
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      published: Int!
+      author: String!
+      genres: [String!]!
+    ): Book
+  }
 `;
 
 const resolvers = {
@@ -137,6 +147,20 @@ const resolvers = {
   },
   Author: {
     bookCount: (root) => books.filter((b) => b.author === root.name).length,
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      if (authors.filter((a) => a.name === args.author).length === 0) {
+        authors = authors.concat({
+          name: args.author,
+          born: null,
+          bookCount: 1,
+        });
+      }
+      const book = { ...args, id: uuid() };
+      books = books.concat(book);
+      return book;
+    },
   },
 };
 

b0ed7793752f0032cad8ca5f1183c35b881f4942
HardJoe <jonathan.amadeus@ui.ac.id>
2022-04-29 16:41:44 +0700

    Implement WebBMI (9.5)

--- part9/fitness/bmiCalculator.ts
+++ part9/fitness/bmiCalculator.ts
@@ -33,11 +33,11 @@ try {
   const { height, weight } = parseArguments(process.argv);
   console.log(calculateBmi(height, weight));
 } catch (error: unknown) {
-  let errorMessage = 'Something bad happened.';
+  let errorMessage: string = 'Something bad happened.';
   if (error instanceof Error) {
     errorMessage += ' Error: ' + error.message;
   }
   console.log(errorMessage);
 }
 
-export {};
+export { calculateBmi };
--- part9/fitness/index.ts
+++ part9/fitness/index.ts
@@ -1,8 +1,28 @@
 import express from 'express';
+import { calculateBmi } from './bmiCalculator';
 const app = express();
 
 app.get('/hello', (_, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  const { height, weight } = req.query;
+  try {
+    const heightNum: number = Number(height);
+    const weightNum: number = Number(weight);
+    if (isNaN(heightNum) || isNaN(weightNum)) {
+      throw new Error();
+    }
+    const bmi: String = calculateBmi(heightNum, weightNum);
+    res.json({
+      weight: weightNum,
+      height: heightNum,
+      bmi,
+    });
+  } catch {
+    res.json({ error: 'malformatted parameters' });
+  }
 });
 
 const PORT = 3003;

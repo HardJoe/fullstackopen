e26a7130b6c88481b7c2cf1582c84b0d3aaf9efc
HardJoe <jonathan.amadeus@ui.ac.id>
2022-05-01 16:02:22 +0700

    Implement patientor backend step3 (9.10)

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,9 +1,8 @@
-import cors from 'cors';
 import express from 'express';
+import diagnosesRouter from './routes/diagnoses';
 
 const app = express();
 
-app.use(cors);
 app.use(express.json());
 
 const PORT = 3001;
@@ -12,6 +11,8 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
